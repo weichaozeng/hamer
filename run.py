e7d370372380ae442ae38cc979803edfe83c58eb
weichaozeng <zvc22@outlook.com>
2025-12-03 08:51:26 +0800

    update run.py

--- run.py
+++ run.py
@@ -27,10 +27,7 @@ from hamer.utils.geometry import aa_to_rotmat, perspective_projection
 from hamer.datasets.vitdet_dataset import ViTDetDataset, DEFAULT_MEAN, DEFAULT_STD
 from hamer.utils.renderer import Renderer, cam_crop_to_full
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
-from ultralytics import YOLO
+from ..ultralytics import YOLO
 import hamer
 
 LIGHT_BLUE = (0.65098039, 0.74117647, 0.85882353)
